f7e0aa91078521d7dfda3f1184aec091b205910c
Lateef Azeez <lateefazeez@Lateefs-MacBook-Pro.local>
2023-10-19 14:43:29 -0600

    update tsconfig to skip lib check

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+export const MONGO_URI =
+  process.env.API_MONGO_URI ||
+  "mongodb+srv://user_2:Kgt9jcuWkrZbayDO@foodorderapp.phvty8w.mongodb.net/?retryWrites=true&w=majority";
+
+// process.env.API_MONGO_URI || "mongodb://localhost:27017/food-ordering-app";
+
+export const APP_SECRET_KEY = process.env.API_APP_SECRET_KEY || "secret";
+export const PORT = process.env.PORT || 8000;
